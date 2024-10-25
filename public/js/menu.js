27de8118d84a46624f5d63f7c0b8af97f70141a7
Jonas Araújo <135404598+sraraujo@users.noreply.github.com>
2024-10-25 13:22:38 -0300

    up

---
+++ public/js/menu.js
@@ -0,0 +1,17 @@
+function abrirMenu(){
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    
+    }else{
+        itens.style.display = 'block'
+    }
+}
+
+function mudarTamanho(){
+    if(window.innerWidth >= 768){
+        itens.style.display = 'block'
+    
+    } else {
+        itens.style.display = 'none'
+    }
+}
---
+++ public/js/questions.js
@@ -0,0 +1,111 @@
+export default [
+  {
+    question: "Qual a capital dos Estados Unidos?",
+    answers: [
+      { option: "Nova Iorque", correct: false },
+      { option: "Califórnia", correct: false },
+      { option: "Washington, D.C.", correct: true },
+      { option: "New Yorque", correct: false },
+      { option: "Florida", correct: false },
+    ],
+  },
+
+  {
+    question: "Em qual continente está localizada Estados Unidos?",
+    answers: [
+      { option: "Europa", correct: false },
+      { option: "África", correct: false },
+      { option: "Ásia", correct: false },
+      { option: "América do Norte", correct: true },
+      { option: "Oceania", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual o idioma oficial dos Estados Unidos?",
+    answers: [
+      { option: "Português", correct: false },
+      { option: "Mandarim", correct: false },
+      { option: "Inglês Americano", correct: true },
+      { option: "Castelhano (castellano)", correct: false },
+      { option: "Francês", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual a moeda monetária dos Estados Unidos?",
+    answers: [
+      { option: "Real", correct: false },
+      { option: "Dólar Americano", correct: true },
+      { option: "Libra", correct: false },
+      { option: "Yuan chinês", correct: false },
+      { option: "Peso Argentino", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual as cores da bandeira dos Estados Unidos?",
+    answers: [
+      { option: "Azul, branco e preto", correct: false },
+      { option: "Amarelo, verde e azul", correct: false },
+      { option: "Azul, vermelho, branco", correct: true },
+      { option: "Verde, preto e branco", correct: false },
+      { option: "Preto, amarelo e azul", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual esporte mais famoso dos Estados Unidos?",
+    answers: [
+      { option: "Boxe", correct: false },
+      { option: "Natação", correct: false },
+      { option: "Basquete", correct: false },
+      { option: "Beisebol", correct: false },
+      { option: "Futebol Americano", correct: true },
+    ],
+  },
+
+  {
+    question: "Qual a religião predominante dos Estados Unidos?",
+    answers: [
+      { option: "Candomblé", correct: false },
+      { option: "Cristianismo", correct: true },
+      { option: "Espirita", correct: false },
+      { option: "Ateísmo", correct: false },
+      { option: "Hinduísmo", correct: false },
+    ],
+  },
+  
+  {
+    question: "Qual a forma de governo dos Estados Unidos?",
+    answers: [
+      { option: "Monarquia Parlamentarista", correct: false },
+      { option: "Semipresidencialista ", correct: false },
+      { option: "Presidencialismo", correct: true },
+      { option: "Parlamentarista", correct: false },
+      { option: "Monarquia", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual o nome do atual presidente dos Estados Unidos?",
+    answers: [
+      { option: "Joe Biden", correct: false },
+      { option: "Joe Biden", correct: true },
+      { option: "Emmanuel Macron", correct: false },
+      { option: "Tayyip Erdogan", correct: false },
+      { option: "Nayib Bukele", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual acontecimento marcou a data de 11 de Setembro de 2001?",
+    answers: [
+      { option: "Inauguração da Estátua da liberdade", correct: false },
+      { option: "Estados Unidos ganhou sua primeira copa do mundo", correct: false },
+      { option: "Atentado terrorista e destruição das torres gêmeas", correct: true },
+      { option: "Lançaram o primerio iPhone", correct: false },
+      { option: "Vitoria na guerra no Vietnã", correct: false },
+    ],
+  }
+];
